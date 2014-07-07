273ac5ead6269aeeb3df47cb678a0e75000ae138
Nestor Júnior <nestor.tmj@gmail.com>
2014-07-07 19:10:55 -0300

    Novo ajuste no insertion mutation (agora ele tenta primeiro criar um novo)

--- src/main/java/com/gearlles/ga/core/mutation/InsertionMutation.java
+++ src/main/java/com/gearlles/ga/core/mutation/InsertionMutation.java
@@ -18,7 +18,7 @@ import com.gearlles.ga.core.Route;
 public class InsertionMutation implements MutationInterface
 {
 	private static final Logger log = LoggerFactory.getLogger(InsertionMutation.class);
-	
+
 	private static Random rand = new Random();
 
 	public Chromosome mutate(Chromosome chromosome)
@@ -39,34 +39,61 @@ public class InsertionMutation implements MutationInterface
 
 				int V = chromosome.getMap().getFleetSize();
 
+				// Se ainda puder criar uma nova rota e a prob for escolhida,
+				// cria
 				if (rand.nextDouble() < 1.0 / (V * 2) && genesSize < chromosome.getMap().getFleetSize())
 				{
-					Route newRoute = new Route(new ArrayList<NodeVrp>(), chromosome.getMap().getVehicleCapacity(),
-							chromosome.getMap().getFunction());
-					newRoute.addNode(node);
-					gene.add(newRoute);
+					addNodeAsNewRoute(chromosome, node);
 				}
 				else
 				{
-					do
+					// Se nao, tenta escolher uma aleatoriamente
+					int newRouteIndex = rand.nextInt(genesSize - 1);
+					Route selectedRoute = gene.get(newRouteIndex == i ? newRouteIndex + 1 : newRouteIndex);
+
+					if (selectedRoute.getLoad() + node.getDemand() <= selectedRoute.getMaxCapacity())
 					{
-						int newRouteIndex = rand.nextInt(genesSize - 1);
-						Route selectedRoute = gene.get(newRouteIndex == i ? newRouteIndex + 1 : newRouteIndex);
-						if(selectedRoute.getLoad() + node.getDemand() <= selectedRoute.getMaxCapacity())
+						selectedRoute.addNode(node);
+					}
+					else
+					{
+						// Caso nao caiba nela, tenta primeiro criar uma nova
+						// rota
+						if (genesSize < chromosome.getMap().getFleetSize())
 						{
-							selectedRoute.addNode(node);
-							break;
+							addNodeAsNewRoute(chromosome, node);
 						}
 						else
 						{
-							log.debug("Trying the insertion again");
+							// Se nao puder criar uma nova, fica tentando achar
+							// uma existente que caiba
+							do
+							{
+								newRouteIndex = rand.nextInt(genesSize - 1);
+								selectedRoute = gene.get(newRouteIndex == i ? newRouteIndex + 1 : newRouteIndex);
+								if (selectedRoute.getLoad() + node.getDemand() <= selectedRoute.getMaxCapacity())
+								{
+									selectedRoute.addNode(node);
+									break;
+								}
+								
+								log.debug("Tentando novamente (se essa mensagem se repetir infinita, problema!)");
+							} while (true);
 						}
-					} while (true);
+					}
 				}
 			}
 		}
 
 		return new Chromosome(gene);
+	}
+
+	private void addNodeAsNewRoute(Chromosome chromosome, NodeVrp node)
+	{
+		Route newRoute = new Route(new ArrayList<NodeVrp>(), chromosome.getMap().getVehicleCapacity(), chromosome
+				.getMap().getFunction());
+		newRoute.addNode(node);
+		chromosome.getGene().add(newRoute);
 	}
 
 }
