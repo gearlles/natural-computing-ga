f406b9ca66e193fe41767d1f93b2ea11d9768fd8
Nestor Júnior <nestor.tmj@gmail.com>
2014-07-07 19:03:07 -0300

    Ajuste no insertion mutation para não permitir que as rotas ultrapassem seu limite de carga

--- src/main/java/com/gearlles/ga/core/mutation/InsertionMutation.java
+++ src/main/java/com/gearlles/ga/core/mutation/InsertionMutation.java
@@ -4,37 +4,64 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sun.util.logging.resources.logging;
 import br.poli.ecomp.nestor.cn.graph.NodeVrp;
 
+import com.gearlles.ga.App;
 import com.gearlles.ga.core.Chromosome;
 import com.gearlles.ga.core.Population;
 import com.gearlles.ga.core.Route;
 
-public class InsertionMutation implements MutationInterface {
+public class InsertionMutation implements MutationInterface
+{
+	private static final Logger log = LoggerFactory.getLogger(InsertionMutation.class);
+	
 	private static Random rand = new Random();
 
-	public Chromosome mutate(Chromosome chromosome) {
+	public Chromosome mutate(Chromosome chromosome)
+	{
 		List<Route> gene = chromosome.getGene();
-		int genesSize = gene.size(); 
-		for (int i = 0; i < genesSize; i++) {
+		int genesSize = gene.size();
+		for (int i = 0; i < genesSize; i++)
+		{
 			Route r = gene.get(i);
-			for(NodeVrp node : r.getNodes()) {
-				if (rand.nextDouble() > Population.mutationRatio) {
+			for (NodeVrp node : r.getNodes())
+			{
+				if (rand.nextDouble() > Population.mutationRatio)
+				{
 					continue;
 				}
-				
+
 				r.getNodes().remove(node);
-				
-				int newRouteIndex = rand.nextInt(genesSize);
-				
+
 				int V = chromosome.getMap().getFleetSize();
-				// Se a prob de criar uma nova rota NAO for sorteada ou a quantidade de rotas for a capacidade maxima 
-				if(rand.nextDouble() > 1.0 / (V * 2) || genesSize >= chromosome.getMap().getFleetSize()) {
-					gene.get(newRouteIndex == i ? newRouteIndex + 1 : newRouteIndex).addNode(node);
-				} else {
-					Route newRoute = new Route(new ArrayList<NodeVrp>(), chromosome.getMap().getVehicleCapacity(), chromosome.getMap().getFunction());
+
+				if (rand.nextDouble() < 1.0 / (V * 2) && genesSize < chromosome.getMap().getFleetSize())
+				{
+					Route newRoute = new Route(new ArrayList<NodeVrp>(), chromosome.getMap().getVehicleCapacity(),
+							chromosome.getMap().getFunction());
 					newRoute.addNode(node);
 					gene.add(newRoute);
+				}
+				else
+				{
+					do
+					{
+						int newRouteIndex = rand.nextInt(genesSize - 1);
+						Route selectedRoute = gene.get(newRouteIndex == i ? newRouteIndex + 1 : newRouteIndex);
+						if(selectedRoute.getLoad() + node.getDemand() <= selectedRoute.getMaxCapacity())
+						{
+							selectedRoute.addNode(node);
+							break;
+						}
+						else
+						{
+							log.debug("Trying the insertion again");
+						}
+					} while (true);
 				}
 			}
 		}
